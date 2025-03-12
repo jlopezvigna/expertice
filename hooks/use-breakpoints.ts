3f91283e6e227b8495175f18306549a71e348b5d
Julian <lopezvignajulian@gmail.com>
2025-03-11 21:43:14 -0400

    fix

--- hooks/use-breakpoints.ts
+++ hooks/use-breakpoints.ts
@@ -6,13 +6,13 @@ import { useEffect, useState } from "react";
  * Inspired by https://usehooks.com/useMedia/
  * https://gist.github.com/gragland/ed8cac563f5df71d78f4a1fefa8c5633
  */
-export function useMediaQuery(query) {
+export function useMediaQuery(query: string) {
   const [matches, setMatches] = useState(false);
   useEffect(
     () => {
       const mediaQuery = window.matchMedia(query);
       setMatches(mediaQuery.matches);
-      const handler = (event) => setMatches(event.matches);
+      const handler = (event: MediaQueryListEvent) => setMatches(event.matches);
       mediaQuery.addEventListener("change", handler);
       return () => mediaQuery.removeEventListener("change", handler);
     },
