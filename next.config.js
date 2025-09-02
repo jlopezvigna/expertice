4cad5b0b3dd7a50322249470ccf9c82a9f7ff2b2
Julian <lopezvignajulian@gmail.com>
2025-09-02 19:15:13 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.NODE_ENV === "production" ? "/expertice" : "",
+  env: {
+    NEXT_PUBLIC_BASE_PATH:
+      process.env.NODE_ENV === "production" ? "/expertice" : "",
+  },
 };
 
 module.exports = nextConfig;
